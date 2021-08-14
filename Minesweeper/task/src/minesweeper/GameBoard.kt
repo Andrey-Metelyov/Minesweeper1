badf66e3ee718cb0e80c472b45d9ad0186254ad1
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-15 01:08:08 +0900

    Stage 5/5: Battle

--- Minesweeper/task/src/minesweeper/GameBoard.kt
+++ Minesweeper/task/src/minesweeper/GameBoard.kt
@@ -5,60 +5,153 @@ import kotlin.math.min
 import kotlin.math.max
 import kotlin.random.Random
 
-class GameBoard(val height: Int, val width: Int, mines: Int) {
-    enum class CELL_STATE(val ch: String) {
+class GameBoard(private val height: Int, private val width: Int, mines: Int) {
+    enum class CellState(val ch: String) {
         EMPTY("."),
+        OPEN("/"),
         MINE("X"),
-        MARK("?")
+        OPEN_MARKED("*"),
+        EMPTY_MARKED("*"),
+        MINE_MARKED("*"),
     }
-    val grid = Array<Array<CELL_STATE>>(height) {
-        Array(width) { CELL_STATE.EMPTY }
+    private val grid = Array(height) {
+        Array(width) { CellState.EMPTY }
     }
+    val marked: Boolean
+        get() = isCorrectlyMarked()
+
+    private fun isCorrectlyMarked(): Boolean {
+        for (row in grid) {
+            for (cell in row) {
+                if (cell == CellState.OPEN_MARKED || cell == CellState.EMPTY_MARKED || cell == CellState.MINE) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
     init {
         if (mines > height * width) {
             throw IllegalArgumentException("Number of mines ($mines) more than can hold ($height x $width = ${height * width}")
         }
         repeat(mines) {
             while (true) {
-                val row = Random.nextInt(0, height - 1)
-                val col = Random.nextInt(0, width - 1)
-                if (grid[row][col] != CELL_STATE.MINE) {
-                    grid[row][col] = CELL_STATE.MINE
+                val row = Random.nextInt(0, height)
+                val col = Random.nextInt(0, width)
+                if (grid[row][col] != CellState.MINE) {
+                    grid[row][col] = CellState.MINE
+//                    System.err.println("mine placed at $row $col")
                     break
                 }
             }
         }
     }
-    fun print() {
+
+    fun print(showMines: Boolean = false) {
+        print(" |")
+        for (i in 1..width) {
+            print(i)
+        }
+        println("|")
+        print("-|")
+        for (i in 1..width) {
+            print("-")
+        }
+        println("|")
         for ((i, row) in grid.withIndex()) {
 //            System.err.println(row.joinToString("") { it.ch })
+            print("${i+1}|")
             for ((j, cell) in row.withIndex()) {
                 print(
-                    if (cell != CELL_STATE.MINE) {
+                    if (cell == CellState.OPEN) {
                         val mines = getMinesAround(i, j)
                         if (mines > 0) {
                             mines
                         } else {
                             cell.ch
                         }
+                    } else if (!showMines && cell == CellState.MINE) {
+                        CellState.EMPTY.ch
                     } else {
                         cell.ch
                     }
                 )
             }
-            println()
+            println("|")
         }
+        print("-|")
+        for (i in 1..width) {
+            print("-")
+        }
+        println("|")
     }
 
     private fun getMinesAround(row: Int, col: Int): Int {
         var count = 0
         for (i in max(0, row - 1)..min(height - 1, row + 1)) {
             for (j in max(0, col - 1)..min(width - 1, col + 1)) {
-                if (grid[i][j] == CELL_STATE.MINE) {
+                if (grid[i][j] == CellState.MINE || grid[i][j] == CellState.MINE_MARKED) {
                     count++
                 }
             }
         }
         return count
+    }
+
+    fun mark(x: Int, y: Int, command: String): Boolean {
+//        if (command == "free" && (grid[x - 1][y - 1] == CELL_STATE.MINE || grid[x - 1][y - 1] == CELL_STATE.MINE_MARKED)) {
+//            return true
+//        }
+        System.err.println(grid[x - 1][y - 1])
+        if (command == "free") {
+            grid[x - 1][y - 1] = when (grid[x - 1][y - 1]) {
+                CellState.MINE_MARKED -> return true
+                CellState.MINE -> return true
+                CellState.EMPTY -> CellState.OPEN
+                CellState.OPEN -> CellState.OPEN
+                CellState.EMPTY_MARKED -> CellState.OPEN
+                CellState.OPEN_MARKED -> CellState.OPEN
+            }
+        } else {
+            grid[x - 1][y - 1] = when (grid[x - 1][y - 1]) {
+                CellState.MINE_MARKED -> CellState.MINE
+                CellState.MINE -> CellState.MINE_MARKED
+                CellState.EMPTY -> CellState.EMPTY_MARKED
+                CellState.OPEN -> CellState.OPEN_MARKED
+                CellState.EMPTY_MARKED -> CellState.EMPTY
+                CellState.OPEN_MARKED -> CellState.OPEN
+            }
+        }
+        if (grid[x - 1][y - 1] == CellState.OPEN) {
+            markAllEmptyNeighbours(x - 1, y - 1)
+        }
+        System.err.println(grid[x - 1][y - 1])
+        return false
+    }
+
+    private fun markAllEmptyNeighbours(row: Int, col: Int) {
+        for (i in max(0, row - 1)..min(height - 1, row + 1)) {
+            for (j in max(0, col - 1)..min(width - 1, col + 1)) {
+                markEmptyNeighbours(i, j)
+            }
+        }
+    }
+
+    private fun markEmptyNeighbours(row: Int, col: Int) {
+//        System.err.println("mark $row $col from ${grid[row][col]}")
+        if (grid[row][col] != CellState.EMPTY && grid[row][col] != CellState.EMPTY_MARKED) {
+            return
+        }
+        grid[row][col] = CellState.OPEN
+        for (i in max(0, row - 1)..min(height - 1, row + 1)) {
+            for (j in max(0, col - 1)..min(width - 1, col + 1)) {
+                if (grid[i][j] == CellState.EMPTY || grid[i][j] == CellState.EMPTY_MARKED) {
+//                    System.err.println("mark $i $j to ${CellState.OPEN}")
+                    grid[i][j] = CellState.OPEN
+                    markAllEmptyNeighbours(i, j)
+                }
+            }
+        }
     }
 }
--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -4,5 +4,19 @@ fun main() {
     print("How many mines do you want on the field? ")
     val mines = readLine()!!.toInt()
     val board = GameBoard(9, 9, mines)
-    board.print()
+    while (true) {
+        board.print()
+        print("Set/unset mines marks or claim a cell as free: ")
+        val (x, y, command) = readLine()!!.split(" ")
+        if (board.mark(y.toInt(), x.toInt(), command)) {
+            board.print(true)
+            println("You stepped on a mine and failed!")
+            break
+        }
+        if (board.marked) {
+            board.print()
+            println("Congratulations! You found all the mines!")
+            break
+        }
+    }
 }
